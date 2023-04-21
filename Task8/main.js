d2f32e041aa7d638ef0e651a30aa3337ef17661a
Alan Martin <alanmartin7007@gmail.com>
2023-04-21 23:19:24 +0530

    Update main.js

--- Task8/main.js
+++ Task8/main.js
@@ -156,15 +156,14 @@ class EvilCircle extends Shape {
 
 }
 
-// define array to store balls and populate it
+
 
 const balls = [];
 
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
+  
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
